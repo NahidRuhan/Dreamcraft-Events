8e22f8c9b88946bdf3005bbbe8603b56001de27c
NahidRuhan <dilal328@gmail.com>
2024-01-14 15:40:26 +0600

    final version

--- src/pages/AboutUs/AboutUs.jsx
+++ src/pages/AboutUs/AboutUs.jsx
@@ -1,4 +1,10 @@
 import { FaFacebook, FaInstagram, FaTwitter } from "react-icons/fa";
+import lopa from '../../assets/lopa.jpeg'
+import shakib from '../../assets/shakib.jpeg'
+import eshad from '../../assets/eshad.jpeg'
+import srk from '../../assets/srk.jpeg'
+import oriana from '../../assets/oriana.jpeg'
+import ishraful from '../../assets/ishraful.jpg'
 
 const AboutUs = () => {
     return (
@@ -12,8 +18,8 @@ const AboutUs = () => {
             <div className="hero min-h-screen bg-base-200 zoom">
   <div className="hero-content text-center">
     <div className="max-w-md">
-    <img className="w-[250px] h-[250px] rounded-full mx-auto" src="https://i.ibb.co/brGrVFB/people1.jpg" alt="" />
-      <h1 className="text-5xl font-bold pt-6">Alfred Molina</h1>
+    <img className="w-[250px] h-[250px] rounded-full mx-auto" src={shakib} alt="" />
+      <h1 className="text-5xl font-bold pt-6">Shakib Ahmed</h1>
       <h1 className="text-sm font-bold pt-6">Event Manager</h1>
       <p className="py-6">The visionary orchestrator of memorable experiences, directing every detail for flawless event execution</p>
       <span className="flex justify-center gap-5"><FaFacebook></FaFacebook><FaTwitter></FaTwitter><FaInstagram></FaInstagram></span>
@@ -26,8 +32,8 @@ const AboutUs = () => {
             <div className="hero min-h-screen bg-base-200 zoom">
   <div className="hero-content text-center">
     <div className="max-w-md">
-    <img className="w-[250px] h-[250px] rounded-full mx-auto" src="https://i.ibb.co/Thj8GZs/people3.webp" alt="" />
-      <h1 className="text-5xl font-bold pt-6">Amy Winehouse</h1>
+    <img className="w-[250px] h-[250px] rounded-full mx-auto" src={lopa} alt="" />
+      <h1 className="text-5xl font-bold pt-6">Sharifa Akter Lopa</h1>
       <h1 className="text-sm font-bold pt-6">Event Coordinator</h1>
       <p className="py-6">The behind-the-scenes maestro, ensuring smooth logistics and flawless execution of events</p>
       <span className="flex justify-center gap-5"><FaFacebook></FaFacebook><FaTwitter></FaTwitter><FaInstagram></FaInstagram></span>
@@ -40,8 +46,8 @@ const AboutUs = () => {
             <div className="hero min-h-screen bg-base-200 zoom">
   <div className="hero-content text-center">
     <div className="max-w-md">
-    <img className="w-[250px] h-[250px] rounded-full mx-auto" src="https://i.ibb.co/HDsPj5h/people5.jpg" alt="" />
-      <h1 className="text-5xl font-bold pt-6">Jack Daniels</h1>
+    <img className="w-[250px] h-[250px] rounded-full mx-auto" src={eshad} alt="" />
+      <h1 className="text-5xl font-bold pt-6">Enamul Eshad</h1>
       <h1 className="text-sm font-bold pt-6">Marketing Specialist</h1>
       <p className="py-6">The creative promoter, crafting strategies that captivate audiences and drive event attendance</p>
       <span className="flex justify-center gap-5"><FaFacebook></FaFacebook><FaTwitter></FaTwitter><FaInstagram></FaInstagram></span>
@@ -54,8 +60,8 @@ const AboutUs = () => {
             <div className="hero min-h-screen bg-base-200 zoom">
   <div className="hero-content text-center">
     <div className="max-w-md">
-    <img className="w-[250px] h-[250px] rounded-full mx-auto" src="https://i.ibb.co/MhSQnVM/people6.jpg" alt="" />
-      <h1 className="text-5xl font-bold pt-6">Guy Ritchie</h1>
+    <img className="w-[250px] h-[250px] rounded-full mx-auto" src={srk} alt="" />
+      <h1 className="text-5xl font-bold pt-6">Shah Rukh Khan</h1>
       <h1 className="text-sm font-bold pt-6">Venue Manager</h1>
       <p className="py-6">The architectural matchmaker, discovering ideal event settings and perfecting the ambiance</p>
       <span className="flex justify-center gap-5"><FaFacebook></FaFacebook><FaTwitter></FaTwitter><FaInstagram></FaInstagram></span>
@@ -68,8 +74,8 @@ const AboutUs = () => {
             <div className="hero min-h-screen bg-base-200 zoom">
   <div className="hero-content text-center">
     <div className="max-w-md">
-    <img className="w-[250px] h-[250px] rounded-full mx-auto" src="https://i.ibb.co/8x46r7f/people4.jpg" alt="" />
-      <h1 className="text-5xl font-bold pt-6">Joshua Samson</h1>
+    <img className="w-[250px] h-[250px] rounded-full mx-auto" src={oriana} alt="" />
+      <h1 className="text-5xl font-bold pt-6">Tanjina Oriana</h1>
       <h1 className="text-sm font-bold pt-6">Logistics Manager</h1>
       <p className="py-6">The precision organizer, orchestrating seamless transport, accommodation, and on-site operations</p>
       <span className="flex justify-center gap-5"><FaFacebook></FaFacebook><FaTwitter></FaTwitter><FaInstagram></FaInstagram></span>
@@ -82,8 +88,8 @@ const AboutUs = () => {
             <div className="hero min-h-screen bg-base-200 zoom">
   <div className="hero-content text-center">
     <div className="max-w-md">
-    <img className="w-[250px] h-[250px] rounded-full mx-auto" src="https://i.ibb.co/smDhgns/people2.webp" alt="" />
-      <h1 className="text-5xl font-bold pt-6">Homer Simpson</h1>
+    <img className="w-[250px] h-[250px] rounded-full mx-auto" src={ishraful} alt="" />
+      <h1 className="text-5xl font-bold pt-6">Ishraful Islam Sium</h1>
       <h1 className="text-sm font-bold pt-6">Vendor Coordinator</h1>
       <p className="py-6">The vendor whisperer, fostering partnerships and ensuring every supplier aligns with the events vision</p>
       <span className="flex justify-center gap-5"><FaFacebook></FaFacebook><FaTwitter></FaTwitter><FaInstagram></FaInstagram></span>
